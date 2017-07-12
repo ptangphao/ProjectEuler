38431f0852efe6e1adfabc3717e42df9100ecc70
Patrick Tangphao <patrick.tangphao@gmail.com>
2017-07-11 23:29:16 -0700

    refactor haskell code for problem 3

--- problem_003/problem_003.hs
+++ problem_003/problem_003.hs
@@ -1,9 +1,6 @@
 maximum_search num = toInteger(truncate(sqrt (fromIntegral num)))
-
+max_search_list num = [2..maximum_search(num)]
 factors num = [ x | x <- [2..maximum_search(num)], num `mod` x == 0]
 prime num = factors num == []
-prime_list list = [x | x <- list, prime(x)]
-
--- prime_list num = filter prime [1..num]
-
+prime_list list = filter prime list
 answer num = last(prime_list(factors(num)))
