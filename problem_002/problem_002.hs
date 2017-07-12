d04b3c1d5d81129dee62f6d2ff08345cc25e59de
Patrick Tangphao <patrick.tangphao@gmail.com>
2017-07-11 21:20:35 -0700

    create working haskell solution

---
+++ problem_002/problem_002.hs
@@ -0,0 +1,19 @@
+-- Create fibonacci list
+
+fib 0 = 1
+fib 1 = 1
+fib n = fib(n-1) + fib(n-2)
+
+newlimit = map fib [1..]
+
+-- Filter for evens only
+evens_only list = [ x | x <- list , even x]
+
+-- Limit list to values under 4000000
+
+allowedfibs list = takeWhile (<4000000) list
+
+-- Sum total
+
+answer = sum(allowedfibs(evens_only(newlimit)))
+
--- problem_002/problem_002.py
+++ problem_002/problem_002.py
@@ -4,4 +4,3 @@ while fibs[-1] + fibs[-2] < 4000000:
 	fibs.append(fibs[-1]+fibs[-2])
 
 print reduce((lambda x, y: x + y),filter(lambda x : x % 2 == 0, fibs))
-git st
