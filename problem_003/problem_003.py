a671caaa617f1c6d269b3f72bc89196d8549bd88
Patrick Tangphao <patrick.tangphao@gmail.com>
2017-07-11 23:35:02 -0700

    add correct spacing to problem 3 files

--- problem_003/problem_003.py
+++ problem_003/problem_003.py
@@ -1,12 +1,12 @@
 def max_prime_factor(number):
-	first_count = 2
-	factors = []
-	while number >= first_count:
-		if number % first_count == 0:
-			factors.append(first_count)
-			number /= first_count
-		else:
-			first_count += 1
-	return max(factors)
+  first_count = 2
+  factors = []
+  while number >= first_count:
+    if number % first_count == 0:
+      factors.append(first_count)
+      number /= first_count
+    else:
+      first_count += 1
+  return max(factors)
 
 print max_prime_factor(600851475143 )
--- problem_003/problem_003.rb
+++ problem_003/problem_003.rb
@@ -1,17 +1,17 @@
 def prime_factors(number)
-	factors = []
-	test_number = 2
-	while number >= test_number
-		if number % test_number == 0
-			number /= test_number
-			factors << test_number
-		else
-			test_number += 1
-		end
-	end
-	return factors
+  factors = []
+  test_number = 2
+  while number >= test_number
+    if number % test_number == 0
+      number /= test_number
+      factors << test_number
+    else
+      test_number += 1
+    end
+  end
+  return factors
 end
 
 puts prime_factors(600851475143).max
 
-# Since we started at the number 2, any factors we get will have to be prime. 
+# Since we started at the number 2, any factors we get will have to be prime.     
