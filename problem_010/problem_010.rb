8d9508c98273e305b1ca74b8b893119668108ea4
Patrick Tangphao <patrick.tangphao@gmail.com>
2017-07-12 01:57:14 -0700

    solve problem 10 with both ruby and haskell

--- problem_010/problem_010.rb
+++ problem_010/problem_010.rb
@@ -1,5 +1,5 @@
 def is_prime?(number)
-  (2...number).each do |t|
+  (2..Math.sqrt(number)).each do |t|
     return false if number % t == 0
   end
   return true
@@ -7,11 +7,14 @@ end
 
 def runner
   primes = []
-  (2...20000000).each do |t|
+  (2...2000000).each do |t|
+    puts t
     primes << t if is_prime?(t)
   end
   return primes.reduce(:+)
 end 
 
+a = Time.now
 puts runner
+puts "#{a - Time.now} seconds passed"
 
