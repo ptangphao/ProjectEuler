129aa5d0824af3797b1c5e98a9619e0a7a223cc5
Patrick Tangphao <patrick.tangphao@gmail.com>
2017-07-25 20:40:51 -0700

    problem 017 in progress

---
+++ problem_017/problem_017.rb
@@ -0,0 +1,25 @@
+base = {
+  1: "one",
+  2: "two",
+  3: "three",
+  4: "four",
+  5: "five",
+  6: "six",
+  7: "seven",
+  8: "eight",
+  9: "nine"
+}
+
+exceptions = {
+  10: "ten",
+  20: "twenty",
+  30: "thirty",
+  40: "forty",
+  50: "fifty",
+  60: "sixty",
+  70: "seventy",
+  80: "eighty",
+  90: "ninety",
+}
+
+# if 
